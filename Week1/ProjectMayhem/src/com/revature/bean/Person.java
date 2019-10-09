9a54a4b3e7f0b275437f2ff7f259152b11e8f815
matt <jason.knighten@revature.com>
2019-10-09 16:01:33 -0400

    wrapped up day 3

--- Week1/ProjectMayhem/src/com/revature/bean/Person.java
+++ Week1/ProjectMayhem/src/com/revature/bean/Person.java
@@ -1,6 +1,8 @@
 package com.revature.bean;
 
 public class Person {
+	
+	
 	//private fields
 	private String name;
 	private int age;
@@ -16,6 +18,7 @@ public class Person {
 		public Person(String name, int age, int weight) {
 			//this. is referring to the object
 			//=name is referring to the parameter
+			System.out.println("inside Person constructor");
 			this.name=name;
 			this.age=age;
 			this.weight=weight;
@@ -42,9 +45,16 @@ public class Person {
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-	@Override
-	public String toString() {
-		return "Person [name=" + name + ", age=" + age + ", weight=" + weight + "]";
-	}
+
 	
+	  @Override public String toString() { return "Person [name=" + name + ", age="
+	  + age + ", weight=" + weight + "]"; }
+	 
+	/*Code blocks- {things between}
+	 * 
+	 */
+	//Instance code block
+	{System.out.println("I'm in an instance code block!");}
+	//Static code block
+	static {System.out.println("Im in a static code block!");}
 }
