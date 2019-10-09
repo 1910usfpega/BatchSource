187b7ed6054fb96704d18315a4b8df0ef943ca22
“ruzzel <your>
2019-10-09 09:44:47 -0400

    updated

---
+++ Week1/ProjectMayhem/src/com/revature/bean/Person.java
@@ -0,0 +1,50 @@
+package com.revature.bean;
+
+public class Person {
+	//private fields
+	private String name;
+	private int age;
+	private int weight;
+	static String homePlanet="Earth";
+	public static String getHomePlanet() {
+		return homePlanet;
+	}
+	public static void setHomePlanet(String homePlanet) {
+		Person.homePlanet = homePlanet;
+	}
+		//Constructors
+		public Person(String name, int age, int weight) {
+			//this. is referring to the object
+			//=name is referring to the parameter
+			this.name=name;
+			this.age=age;
+			this.weight=weight;
+		}
+		//No-arg Constructor
+		public Person() {
+			//super(); implicitly there
+		}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + ", weight=" + weight + "]";
+	}
+	
+}
