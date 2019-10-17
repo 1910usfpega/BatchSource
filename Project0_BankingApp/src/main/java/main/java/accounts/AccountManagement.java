4cd6692f3a495faa70c693bc91d85d59057cd16a
“Kristhian_Deoliveira” <kristhian.deoliveira@gmail.com.com>
2019-10-17 16:38:34 -0400

    Bank account single, and joint created

--- Project0_BankingApp/src/main/java/main/java/accounts/AccountManagement.java
+++ Project0_BankingApp/src/main/java/main/java/accounts/AccountManagement.java
@@ -10,7 +10,6 @@ import main.java.users.UserAccountManager;
 
 public class AccountManagement {
 	public static void CreateSinglePersonAccount(String username) {
-		if (UserAccountManager.isAccountValid(username)==true) {
 			Scanner input = new Scanner(System.in);
 			System.out.println("What kind of account would you like to create?");
 			System.out.println("1. Checking\n 2. Savings");
@@ -20,20 +19,46 @@ public class AccountManagement {
 				case 1:
 					//Checking
 					writer.append("Username: "+username+" %Checking: $0.00 \n");
+					writer.close();
 					break;
 				case 2:
 					//Savings
 					writer.append("Username: "+username+" %Savings: $0.00 \n");
+					writer.close();
 					break;
 				default:
+					System.out.println("Invalid option.");
 					break;
 				}
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else {
-			
-		}
 	}
+	public static void CreateJointAccount(String username01, String username02) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("What kind of account would you like to create?");
+		System.out.println("1. Checking\n 2. Savings");
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
+			switch (input.nextInt()) {
+			case 1:
+				//Checking
+				writer.append("Username: Joint"+username01+"/"+username02+" %Checking: $0.00 \n");
+				writer.close();
+				break;
+			case 2:
+				//Savings
+				writer.append("Username: Joint"+username01+"/"+username02+" %Savings: $0.00 \n");
+				writer.close();
+				break;
+			default:
+				System.out.println("Invalid option.");
+				break;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+}
 }
