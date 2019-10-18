ac231ae39c5fd09a7e71014fd7fa6e89941e01e3
“Kristhian_Deoliveira” <kristhian.deoliveira@gmail.com.com>
2019-10-18 15:46:11 -0400

    Account management stuff

---
+++ Project0_BankingApp/src/main/java/main/java/accounts/Account.java
@@ -0,0 +1,46 @@
+package main.java.accounts;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String accountType;
+	private String username;
+	private Double accountBalance;
+	//Constructor
+	public Account(String accountType, String username, Double accountBalance) {
+		super();
+		this.accountType = accountType;
+		this.username = username;
+		this.accountBalance = accountBalance;
+	}
+	//Custom Constructor
+	public Account(String username, Double accountBalance) {
+		super();
+		this.username = username;
+		this.accountBalance = accountBalance;
+	}
+	//No-arg constructor
+	public Account() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getAccountType() {
+		return accountType;
+	}
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Double getAccountBalance() {
+		return accountBalance;
+	}
+	public void setAccountBalance(Double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+	
+}
--- Project0_BankingApp/src/main/java/main/java/accounts/AccountManagement.java
+++ Project0_BankingApp/src/main/java/main/java/accounts/AccountManagement.java
@@ -2,135 +2,84 @@ package main.java.accounts;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.security.PublicKey;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import main.java.bean.User;
 import main.java.programstart.ProgramStart;
-import main.java.users.UserAccountManager;
 
 public class AccountManagement {
-	public static void CreateSinglePersonAccount(String username) {
-			Scanner input = new Scanner(System.in);
-			System.out.println("What kind of account would you like to create?");
-			System.out.println("1. Checking\n 2. Savings");
-			try {
-				BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
-				switch (input.nextInt()) {
-				case 1:
-					//Checking
-					writer.append("Username: "+username+" %Checking: $0.00 \n");
-					writer.close();
-					System.out.println("Checking account created successfully! \\n 1. Go to main menu \\n2. Add funds \\n");
-					Scanner userInput = new Scanner(System.in);
-					switch (userInput.nextInt()) {
-					case 1:
-						ProgramStart.startProgram();
-						break;
-					case 2:
-						System.out.println("How much would you like to deposit?");
-						Scanner getAmt = new Scanner(System.in);
-						AddfundsToCheckingAccount(username, getAmt.nextDouble());
-					default:
-						break;
-					}
-					break;
-				case 2:
-					//Savings
-					writer.append("Username: "+username+" %Savings: $0.00 \n");
-					writer.close();
-					System.out.println("Savings account created successfully! \n 1. Go to main menue \n2. Add funds \n");
-					Scanner userInput02 = new Scanner(System.in);
-					switch (userInput02.nextInt()) {
-					case 1:
-						ProgramStart.startProgram();
-						break;
-					case 2:
-						System.out.println("How much would you like to deposit?");
-						Scanner getAmt = new Scanner(System.in);
-						AddfundsToSavingsAccount(username, getAmt.nextDouble());
-						break;
-					default:
-						break;
-					}
-					break;
-				default:
-					System.out.println("Invalid option.");
-					break;
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-	public static void CreateJointAccount(String username01, String username02) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("What kind of account would you like to create?");
-		System.out.println("1. Checking\n 2. Savings");
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
-			switch (input.nextInt()) {
-			case 1:
-				//Checking
-				writer.append("Username: Joint"+username01+"/"+username02+" %Checking: $0.00 \n");
-				writer.close();
-				break;
-			case 2:
-				//Savings
-				writer.append("Username: Joint"+username01+"/"+username02+" %Savings: $0.00 \n");
-				writer.close();
-				break;
-			default:
-				System.out.println("Invalid option.");
-				break;
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-}
-	public static void AddfundsToCheckingAccount(String Username, double ammount) {
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
-			Scanner reader = new Scanner(new File("Accounts.txt"));
-			do {
-				String line = reader.nextLine();
-				double currentLineValue = reader.nextDouble();
-				if (line.contains(Username) && line.contains("%Checking")) {
-					currentLineValue+=ammount;
-					line.replaceFirst(Double.toString(currentLineValue), Double.toString(ammount));
-				} else {
-					System.out.println("The username has no checking account.");
-					
-				}
-			} while (reader.hasNextLine());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
 	
-	public static void AddfundsToSavingsAccount(String Username, double ammount) {
-		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter("Accounts.txt", true));
-			Scanner reader = new Scanner(new File("Accounts.txt"));
-			do {
-				String line = reader.nextLine();
-				double currentLineValue = reader.nextDouble();
-				if (line.contains(Username) && line.contains("%Savings")) {
-					currentLineValue+=ammount;
-					line.replaceFirst(Double.toString(currentLineValue), Double.toString(ammount));
-				} else {
-					System.out.println("The username has no checking account.");
-					
-				}
-			} while (reader.hasNextLine());
+	public static final String accountFile = "Account.txts";
+	public static List<Account> accountList = new ArrayList<Account>();
+	//Write accounts to file
+	public static void writeAccountFile() {
+		try(ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream(accountFile));){
+			objectOut.writeObject(accountList);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
+	}
+	//Read accounts to file
+	public static void readAccountFile() {
+		try(ObjectInputStream objectIn = new ObjectInputStream(new FileInputStream(accountFile));){
+			accountList = (ArrayList<User>) objectIn.readObject();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//Create a checking account for a single person
+	public static void CreateSinglePersonCheckingAccount(String username) {
+		System.out.println("Please enter initial deposit ammount.");
+		Scanner scanner = new Scanner(System.in);
+		accountList.add(new Account("Checking", username, scanner.nextDouble()));
+		System.out.println("Account created.");
+		writeAccountFile();
+		ProgramStart.customerMenu(username);
+	}
+	//Create a Joint checking account between two people
+	public static void CreateJointCheckingAccount(String usernameA, String usernameB) {
+		System.out.println("Please enter initial deposit ammount.");
+		Scanner scanner = new Scanner(System.in);
+		accountList.add(new Account("Checking", usernameA+"/"+usernameB, scanner.nextDouble()));
+		System.out.println("Account created.");
+		ProgramStart.customerMenu(usernameA);
+	}
+	//Create a one person savings account.
+	public static void CreateSinglePersonSavingsAccount(String username) {
+		System.out.println("Please enter initial deposit ammount.");
+		Scanner scanner = new Scanner(System.in);
+		accountList.add(new Account("Savings", username, scanner.nextDouble()));
+		System.out.println("Account created.");
+		writeAccountFile();
+		ProgramStart.customerMenu(username);
+	}
+	//Create a joint savings account between two people.
+	public static void CreateJointSavingsAccount(String usernameA, String usernameB) {
+		System.out.println("Please enter initial deposit ammount.");
+		Scanner scanner = new Scanner(System.in);
+		accountList.add(new Account("Savings", usernameA+"/"+usernameB, scanner.nextDouble()));
+		System.out.println("Account created.");
+		ProgramStart.customerMenu(usernameA);
 	}
 }
