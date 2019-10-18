f6b9bde26151ba9b256aa7f116dce23c9f7d1cea
lana-del-rey <iskandar@inbox.ru>
2019-10-18 16:11:58 -0400

    login fuction created

---
+++ BankingApp/src/com/revature/bean/User.java
@@ -0,0 +1,62 @@
+package com.revature.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class User implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+	private String userType;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserType() {
+		return userType;
+	}
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+	
+	public User(String username, String password, String userType) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.userType = userType;
+	}
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", userType=" + userType + "]";
+	}
+	
+	
+	public static User login(String username, String password, List<User> usersList) throws LoginException {
+		for (User user:usersList) {
+			if (user.getUsername().toString().equals(username)) {
+				if (user.getPassword().toString().equals(password)) {
+					return user;
+				} else {
+					throw new LoginException("Incorrect pass");
+				}
+			}
+		}
+		throw new LoginException("No such login: " + username);
+	}
+	
+	
+	
+	
+}
--- BankingApp/src/com/revature/driver/Driver.java
+++ BankingApp/src/com/revature/driver/Driver.java
@@ -2,48 +2,75 @@ package com.revature.driver;
 
 import java.util.Scanner;
 
+import com.revature.bean.LoginException;
+import com.revature.bean.User;
+import com.revature.io.IOWithCollections;
+
 public class Driver {
 	public static Scanner sc = new Scanner(System.in);
+	public static User currentUser;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String contents = null;
-		System.out.println("What would you like to do?\nLogin press 1\nLogon press 2");
-		System.out.println(sc.hasNextInt());
-		//(contents = sc.nextInt()) !=0
-		while ((contents = sc.nextLine())!= null) {
-			if (contents.equals("1") || contents.equals("2")) {
-				break;
-			} else {
-				System.out.println("Input 1 or 2");
-			}
-		} 
 		
-		System.out.println("Outside while" + contents);
+		IOWithCollections.readUserFile();
 		
 		
-		if (contents.equals("1")) {
-			System.out.println("Login process");
-		} else if (contents.equals("2")) {
-			System.out.println("Logon process");
+		String login = "username1";
+		String pass = "pass1";
+		
+		try {
+			currentUser = User.login(login, pass, IOWithCollections.usersList);
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-//		int contents;
+		
+		System.out.println("Successfull login: " + currentUser.getUsername());
+		
+		
+		
+		
+		
+		
+//		User u1 = new User("username1", "pass", "customer");
+//		User u2 = new User("username2", "pass", "customer");
+//		User u3 = new User("username3", "pass", "customer");
+//		
+//		IOWithCollections.usersList.add(u1);
+//		IOWithCollections.usersList.add(u2);
+//		IOWithCollections.usersList.add(u3);
+//		
+//		IOWithCollections.writeUserFile();
+//		 TODO Auto-generated method stub
+		
+		// Read Customers, Accounts and Transactions from file
+		
+		
+		// Check if we already logged in
+		
+		
+		
+		
+		
+//		String contents = null;
 //		System.out.println("What would you like to do?\nLogin press 1\nLogon press 2");
-//		while (!sc.hasNextInt()) {
-//			if (sc.hasNextInt()) {
-//				contents = sc.nextInt();
-//				if (contents == 1) {
-//					System.out.println("Login process");
-//				} else if (contents == 2) {
-//					System.out.println("logon process");
-//				} else {
-//					
-//				}
+//		System.out.println(sc.hasNextInt());
+//		//(contents = sc.nextInt()) !=0
+//		while ((contents = sc.nextLine())!= null) {
+//			if (contents.equals("1") || contents.equals("2")) {
+//				break;
 //			} else {
-//				System.out.println("press 1 or 2 ");
+//				System.out.println("Input 1 or 2");
 //			}
-//			
+//		} 
+//		
+//		
+//		if (contents.equals("1")) {
+//			System.out.println("Start Login process");
+//		} else if (contents.equals("2")) {
+//			System.out.println("Start Logon process");
 //		}
+
 		
 	}
 
