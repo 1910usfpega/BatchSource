b3b62c553f462f3d973ee717cf8aca6e6b85b97a
NarrainenS <you@example.com>
2019-10-18 21:36:34 -0400

    class Account

--- BankingApp/src/com/revature/bean/Account.java
+++ BankingApp/src/com/revature/bean/Account.java
@@ -4,6 +4,9 @@ public class Account {
 	private String accountName;
 	private Double balance;
 	private String ownerUsername;
-		
+	private String accountStatus;
+	private Integer accountNumber;
+
+	
 	
 }
