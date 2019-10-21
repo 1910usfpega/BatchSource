f2be940da8aaa340da69453abda88917dc32d78d
jtodd-3 <james.todd927@gmail.com>
2019-10-21 11:20:39 -0400

    updated Save file

--- bankApp/src/com/revature/io/Save.java
+++ bankApp/src/com/revature/io/Save.java
@@ -1,9 +1,10 @@
 package com.revature.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import com.revature.bean.Customer;
@@ -13,47 +14,53 @@ import com.revature.bean.bankAdmin;
 
 public class Save {
 
-	//Write all collections to corresponding files
-	//If corresponding files do not exist, create them upon first execution
-	public static File adminFile = new File("admins.txt");
-	public static File employeeFile = new File("employees.txt");
-	public static File customerFile = new File("customers.txt");
-	public static File pendingAccountFile = new File("pendingAccounts.txt");
-	
-	public static void save(ArrayList<Customer> cus, ArrayList<Employee> emp,
-							ArrayList<bankAdmin> adm, ArrayList<PendingAccount> pen) {
-		
-		//Write all admins to admin.txt
-		try(OutputStream os = new FileOutputStream(adminFile, false)) {
-			os.write(adm.toString().getBytes());
-			System.out.println("Admin data saved successfully");
-			
+	// Write all collections to corresponding files
+	// If corresponding files do not exist, create them upon first execution
+	public static final File adminFile = new File("admins.txt");
+	public static final File employeeFile = new File("employees.txt");
+	public static final File customerFile = new File("customers.txt");
+	public static final File pendingAccountFile = new File("pendingAccounts.txt");
+
+	public static void save(ArrayList<Customer> cus, ArrayList<Employee> emp, ArrayList<bankAdmin> adm,
+			ArrayList<PendingAccount> pen) {
+
+		// Write all admins in admin collection to admin.txt
+		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(adminFile, false));) {
+			objOut.writeObject(adm);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//Write all employees to employee.txt
-		try(OutputStream os = new FileOutputStream(employeeFile, false)) {
-			os.write(emp.toString().getBytes());
+
+		// Write all employees to employee.txt
+		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(employeeFile, false));) {
+			objOut.writeObject(emp);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//Write all customers to customer.txt
-		try(OutputStream os = new FileOutputStream(employeeFile, false)) {
-			os.write(cus.toString().getBytes());
+
+		// Write all customers to customer.txt
+		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(employeeFile, false));) {
+			objOut.writeObject(cus);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		//Write all pending accounts to pending.txt
-		try(OutputStream os = new FileOutputStream(pendingAccountFile, false)) {
-			os.write(pen.toString().getBytes());
+
+		// Write all pending accounts to pending.txt
+		try (ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream(pendingAccountFile, false));) {
+			objOut.writeObject(pen);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
-	
+
 }
