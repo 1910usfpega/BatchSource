5ee45df4bcf0a9508803e5d245e338d6db6f3dfe
matt <jason.knighten@revature.com>
2019-10-24 15:38:17 -0400

    Wrap up day 4

--- SQL/JDBCDemo/src/main/java/com/revature/driver/Driver.java
+++ SQL/JDBCDemo/src/main/java/com/revature/driver/Driver.java
@@ -3,14 +3,17 @@ package com.revature.driver;
 import java.sql.SQLException;
 
 import com.revature.daoimpl.AlbumDaoImpl;
+import com.revature.daoimpl.StudentDaoImpl;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		AlbumDaoImpl adi = new AlbumDaoImpl();
+		StudentDaoImpl sdi= new StudentDaoImpl();
 		try {
 			//System.out.println(adi.getAllAlbums());
-			System.out.println(adi.getAlbumsById(22));
+			//System.out.println(adi.getAlbumsById(22));
+			sdi.insertStudent("Ryan pretty", 2);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
