21f5b4df13debb6c0cacf2f1636f6d2f0b1985c3
“jenningsj <jjennings1125@gmail.com>
2019-10-24 10:08:59 -0400

    Updating from Matts branch

---
+++  JohnsWeekOne/ProjectMayhem/src/com/revature/chaining/Mouse.java
@@ -0,0 +1,38 @@
+package com.revature.chaining;
+//Constructor Chaining
+public class Mouse {
+	private int numTeeth;
+	private int numWhiskers;
+	private int weight;
+	
+	
+	@Override
+	public String toString() {
+		return "Mouse [numTeeth=" + numTeeth + ", numWhiskers=" + numWhiskers + ", weight=" + weight + "]";
+	}
+
+
+	public Mouse() {
+		super();
+	}
+
+
+	public Mouse(int numTeeth, int numWhiskers, int weight) {
+		super();
+		this.numTeeth = numTeeth;
+		this.numWhiskers = numWhiskers;
+		this.weight = weight;
+	}
+	
+	public Mouse(int numTeeth, int numWhiskers) {
+		this(numTeeth,numWhiskers,16);
+		
+	}
+	public Mouse(int numTeeth) {
+		this(numTeeth,8);
+	}
+	public static void main(String[] args) {
+		Mouse m= new Mouse();
+		System.out.println(m.toString());
+	}
+}
---
+++  JohnsWeekOne/TheFirstStep/src/com/revature/secondstep/TheThirdStep.java
@@ -0,0 +1,9 @@
+package com.revature.secondstep;
+
+public class TheThirdStep {
+	
+	
+	public static void main(String [] args) {
+		System.out.println("Goodbye, Earth");
+	}
+}
