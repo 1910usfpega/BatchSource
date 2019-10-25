eaa17460896bb93f618abcbd0e2581ba0c44e042
Ben <benright313@gmail.com>
2019-10-24 20:49:50 -0400

    Project 1

---
+++ Project 1/BenrightsBank/src/main/java/com/revature/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.revature.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.revature.beans.User;
+
+public interface UserDao {
+//CRUD OPS
+	public List<User> getAllUsers() throws SQLException;
+	public List<User> getUsersByAccountId(int accountId) throws SQLException;
+	
+}
