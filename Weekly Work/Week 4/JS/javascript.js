3dad94904c459e0b2d270b9ac1e1a1e9b10c7a26
Ben <benright313@gmail.com>
2019-10-29 16:41:41 -0400

    One more time

---
+++ Weekly Work/Week 4/JS/javascript.js
@@ -0,0 +1,42 @@
+var a = 10;
+console.log(a);
+a = "chaos";
+console.log(a);
+var b, c, d, e, f, g, h, e;
+b = "10";
+c = true;
+d = {}; //object
+e = null;
+g = (0/0);
+h = []; //array
+i = function(){};
+/* Datatypes
+    primitives
+        number
+        string
+        undefined
+        symbol
+        null
+        boolean
+            true or false
+            ALSO have truthy falsy
+                Falsy
+                    0
+                    undefined
+                    null
+                    false
+                    ""
+                    NaN
+                Truthy
+                    true
+                    1
+                    everything that isn't falsy
+                    Object
+    Objects
+
+*/
+//Type coersion
+console.log(5==5);
+console.log(5=="5");
+console.log(5==="5");
+console.log(2+3=="5");
